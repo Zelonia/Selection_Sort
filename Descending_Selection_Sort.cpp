a92cbb7173a43027b0fba7a9060b3083ae34e2c4
Zelonia <119892932+Zelonia@users.noreply.github.com>
2023-05-24 22:34:13 +0700

    Add files via upload

---
+++ Descending_Selection_Sort.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+	int data[50], swap, terkecil, input, lagi;
+	
+	awal:
+	system("cls");	
+	cout<<"SELECTION SORT DESCENDING"<<endl;
+    cout<<"--------------------------"<<endl;
+    
+    
+	cout<<"Masukkan banyak bilangan : ";
+	cin>>input;
+	
+	
+	for(int i=0; i<input; i++){
+		cout<<"--------------------------"<<endl;
+		cout<<"Input Data Ke-"<<(i+1)<<" = " ;
+		cin>>data[i];
+		
+	}
+	
+	system("cls");
+	cout<<endl;
+	cout<<"Data Yang Sebelum Di Selection  : \n"<<endl;
+	for(int i=0; i<input; i++){
+
+		cout<<data[i]<<"  ";
+	}
+	
+	cout<<"\n\n---------------------------------------------"<<endl;
+	cout<<"  T A H A P   S E L E C T I O N"<<endl;
+	cout<<"---------------------------------------------"<<endl;
+	for(int i=0; i<input; i++){
+		terkecil=i;
+		for(int j=i+1; j<input; j++){
+			if(data[j] > data[terkecil])
+			terkecil=j;
+		}
+		if(data[i] != data[terkecil]){
+			swap = data[i];
+			data[i] = data[terkecil];
+			data[terkecil] = swap;
+			
+		}
+		cout<<endl;
+		cout<<"Tahap Selection ke "<<i+1<<" : ";
+		for(int j=0; j<input; j++){
+			cout<<data[j]<<" ";
+			
+		}
+		cout<<"\n";
+	}
+	
+	cout<<"\n\n---------------------------------------------"<<endl;
+	cout<<"Hasi Data Yang Sudah Di Selection \n"<<endl;
+	for(int i=0; i<input; i++){
+		cout<<data[i]<<"  ";
+		}
+	
+	cout<<"\n---------------------------------------------\n"<<endl;
+	cout<<"\n\nApakah Anda Ingin Menginput data lagi ?(1/2)"<<endl;
+    cin>>lagi;
+    if(lagi==1)
+    {
+        goto awal;
+    }
+    else{
+        goto akhir;
+    }
+akhir:
+		
+		
+	return 0;
+}
+
